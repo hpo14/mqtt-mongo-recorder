17cb6d5fdd03ac2e75f45c7daea28f88026c91aa
jopyhenriques <jopy.henriques@gmail.com>
2017-10-01 07:35:57 -0400

    Updated config.js
    Had to change the calls to environment variables.

--- config.js
+++ config.js
@@ -8,9 +8,11 @@ config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'iot.eclipse.org';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
-config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'localhost';
-config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
-config.mongodb.database   = process.env.MONGODB_DATABASE   || 'mqtt';
-config.mongodb.collection = process.env.MONGODB_COLLECTION || 'message';
+config.mongodb.hostname   = process.env.MYMONGO_SERVICE_HOST    || 'localhost';
+config.mongodb.port       = process.env.MYMONGO_SERVICE_PORT    || 27017;
+config.mongodb.database   = process.env.MONGODB_DATABASE        || 'dbmqtt';
+config.mongodb.collection = process.env.MONGODB_COLLECTION      || 'message';
+config.mongodb.user = process.env.MONGODB_USER;
+config.mongodb.password = process.env.MONGODB_PASSWORD;
 
 module.exports = config;
