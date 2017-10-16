cd03c7dcda9e1daaae666da06eef397db6c66a1d
Niel Eyde <nieleyde@gmail.com>
2017-10-15 21:18:13 -0400

    added code change

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ mongodb.MongoClient.connect(mongoUri, function(error, database) {
         var messageObject = {
             ts: new Date(),
             topic: topic,
+            //container_os_host_name: os.hostname(),
+            //container_os_platform: os.platform(),
             message: message.toString(),
             json: json
         };
